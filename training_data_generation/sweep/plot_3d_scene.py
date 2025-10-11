544db75bb3e74d3b74abcfadead1689b2d16647c
Mikkel <jensenen@live.dk>
2025-10-11 21:10:04 +0200

    3D animation of radar sweep

--- training_data_generation/sweep/plot_3d_scene.py
+++ training_data_generation/sweep/plot_3d_scene.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FFMpegWriter
+import io
+# from mpl_toolkits.mplot3d import Axes3D
 
+import matplotlib
+matplotlib.use('Agg')
 
-def plot_radar_scene(radar, targets, show_pattern=False, pulse_idx=None):
+def plot_radar_scene(radar, targets, show_pattern=False, pulse_idx=None, t_offset=0):
     """
     Plot a 3D radar scene for a RadarSimPy radar object.
 
@@ -22,6 +26,7 @@ def plot_radar_scene(radar, targets, show_pattern=False, pulse_idx=None):
     pulse_idx : int or None, optional
         If int  -> display snapshot for that pulse.
         If None -> display for the center pulse.
+    t_offset : adds offset to time displayed in title in seconds
     """
 
     fig = plt.figure(figsize=(10, 8))
@@ -76,15 +81,47 @@ def plot_radar_scene(radar, targets, show_pattern=False, pulse_idx=None):
         v = np.array(tgt["speed"])
         p = p0 + v * t_sel  # position at current pulse
         ax.scatter(*p, marker="x", s=60, label=f"Target {i}")
-        ax.plot([p0[0], p[0]], [p0[1], p[1]], [p0[2], p[2]], "k--", alpha=0.4)
+        # Draw a short line in the target’s velocity direction (forward only)
+        dir_scale = 1  # [s] scale factor for visualization, adjust for clarity
+        p_dir = p + (v /  np.linalg.norm(v)) * dir_scale  # extend only forward in velocity direction
+        ax.plot([p[0], p_dir[0]], [p[1], p_dir[1]], [p[2], p_dir[2]], "k--", alpha=0.6)
 
     # --- Labels ---
     ax.set_xlabel("X [m]")
     ax.set_ylabel("Y [m]")
     ax.set_zlabel("Z [m]")
     ax.legend()
-    ax.set_title(f"Radar 3D Scene @ Pulse {pulse_idx} (t={t_sel*1e6:.2f} µs)")
+    ax.set_title(f"Radar 3D Scene @ Pulse {pulse_idx} (t={(t_offset + t_sel)*1e6:.2f} µs)")
 
     ax.set_box_aspect([1, 1, 1])
-    plt.tight_layout()
-    plt.show()
+    ax.set_xlim(-11, 11)
+    ax.set_ylim(-11, 11)
+    ax.set_zlim(-11, 11)
+
+    fig.canvas.draw()
+    buf = io.BytesIO()
+    fig.canvas.print_rgba(buf)
+    buf.seek(0)
+    image = np.frombuffer(buf.getvalue(), dtype='uint8')
+    image = image.reshape(fig.canvas.get_width_height()[::-1] + (4,))
+    image = image[..., :3]  # drop alpha if you want RGB
+    plt.close(fig)
+
+    return image
+
+def save_frames_mp4(frames, path=None):
+    print("Saving frames...")
+
+    # Save frames as a video
+    metadata = dict(title='RADAR 3D Animation')
+    writer = FFMpegWriter(fps=10, metadata=metadata)
+
+    fig = plt.figure()
+    plt.axis('off')  # hide axes for video
+    if path is None:
+        path = "3d_movie.mp4"
+
+    with writer.saving(fig, path, dpi=150):
+        for frame in frames:
+            plt.imshow(frame)
+            writer.grab_frame()
--- training_data_generation/sweep/sweep_radar.py
+++ training_data_generation/sweep/sweep_radar.py
@@ -1,10 +1,14 @@
+import numpy as np
+import math
+from tqdm import tqdm
+
 from radiation_pattern import radiation_pattern
-from plot_3d_scene import  plot_radar_scene
+from plot_3d_scene import  plot_radar_scene, save_frames_mp4
 
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from radarsimpy import Radar, Transmitter, Receiver
-# from radarsimpy.simulator import sim_radar
+from radarsimpy.simulator import sim_radar
 
 c = 3e8
 f_c = 5.8e9 # center frequency
@@ -17,9 +21,6 @@ pulses = 128
 
 fs = 45e6 # 50e6 # IF fs
 
-hpbw_el_deg = 27
-hpbw_az_deg = 88
-
 r_max = (c * t_chirp) / 2 # calculate the maximum range
 delta_R = c / (2 * bw)  # Calculate range resolution (meters / bin)
 doppler_max = wavelength / (4 * prp) #((wavelength * (1 / (2 * prp))) / 2)
@@ -29,68 +30,124 @@ delta_velocity = wavelength / (2 * pulses * prp)
 # print(f"max velocity {round(doppler_max, 2)} m/s; velocity resolution: {round(delta_velocity, 3)} m/s")
 # print(f"tx time: {prp * pulses}s; sampls/chirp: {round(t_chirp * fs, 2)}")
 
+def frame_simulation(target_list, hpbw_az_deg, hpbw_el_deg, t_offset=0, steering_angle_deg=0, include_noise=False, render=False):
+    fig = None
+    baseband = None
+    end_of_last_pulse = None
 
-el_angle, el_gain = radiation_pattern(hpbw_el_deg, include_sidelobes=True, steering_angle_deg=0)
-az_angle, az_gain = radiation_pattern(hpbw_az_deg, include_sidelobes=False)
+    el_angle, el_gain = radiation_pattern(hpbw_az_deg, include_sidelobes=False)
+    az_angle, az_gain = radiation_pattern(hpbw_el_deg, include_sidelobes=True, steering_angle_deg=steering_angle_deg)
 
-N_tx = 1
-N_rx = 4
+    N_tx = 1
+    N_rx = 4
 
-tx_channels = []
-if (N_tx == 1):
-    tx_channels.append(dict(
-        location=(0, 0, 0), 
-        azimuth_angle=az_angle,
-        azimuth_pattern=az_gain,
-        elevation_angle=el_angle,
-        elevation_pattern=el_gain,
-    ))
-else:
-    for idx in range(0, N_tx):
+    tx_channels = []
+    if (N_tx == 1):
         tx_channels.append(dict(
-            location=(0, wavelength / 2 * idx - (N_tx - 1) * wavelength / 4, 0),
+            location=(0, 0, 0), 
             azimuth_angle=az_angle,
             azimuth_pattern=az_gain,
             elevation_angle=el_angle,
             elevation_pattern=el_gain,
         ))
+    else:
+        for idx in range(0, N_tx):
+            tx_channels.append(dict(
+                location=(0, wavelength / 2 * idx - (N_tx - 1) * wavelength / 4, 0),
+                azimuth_angle=az_angle,
+                azimuth_pattern=az_gain,
+                elevation_angle=el_angle,
+                elevation_pattern=el_gain,
+            ))
 
 
-rx_channels = []
-for idx in range(0, N_rx):
-    rx_channels.append(
-        dict(
-            location=(0, wavelength / 2 * idx - (N_rx - 1) * wavelength / 4, 0),
+    rx_channels = []
+    for idx in range(0, N_rx):
+        rx_channels.append(
+            dict(
+                location=(0, wavelength / 2 * idx - (N_rx - 1) * wavelength / 4, 0),
+            )
         )
+
+    tx = Transmitter(
+        f=[f_c - (bw/2), f_c + (bw/2)],
+        t=[0, t_chirp],
+        tx_power=40, # 40
+        prp=prp,
+        pulses=pulses,
+        channels=tx_channels
     )
 
-tx = Transmitter(
-    f=[f_c - (bw/2), f_c + (bw/2)],
-    t=[0, t_chirp],
-    tx_power=40, # 40
-    prp=prp,
-    pulses=pulses,
-    channels=tx_channels
-)
+    rx = Receiver(
+        fs=fs,
+        noise_figure=0, # 8
+        rf_gain=20,
+        load_resistor=50,
+        baseband_gain=30,
+        channels=rx_channels
+    )
 
-rx = Receiver(
-    fs=fs,
-    noise_figure=0, # 8
-    rf_gain=20,
-    load_resistor=50,
-    baseband_gain=30,
-    channels=rx_channels
-)
+    radar = Radar(transmitter=tx, receiver=rx)
 
-radar = Radar(transmitter=tx, receiver=rx)
+    wf = radar.radar_prop["transmitter"].waveform_prop
+    pulse_start = wf["pulse_start_time"]
+    wf_prp = wf["prp"]
+    end_of_last_pulse = pulse_start[-1] + wf_prp[-1]
+
+    # calculate new target location based on t_offset
+    for target in target_list:
+        target["location"] = tuple([target["location"][i] + (target["speed"][i] * t_offset) for i in range(3)])
+
+    # data = sim_radar(radar, target_list)
+    # if include_noise:
+    #     baseband = data["baseband"] + data["noise"]
+    # else:
+    #     baseband = data["baseband"]
+
+    if render:
+        fig = plot_radar_scene(radar, target_list, show_pattern=True, pulse_idx=None, t_offset=t_offset)
+
+    return (baseband, end_of_last_pulse, fig)
+
+
+def sweep_simulation(target_list, hpbw_az_deg, hpbw_el_deg, steering_angles=[], include_noise=False, render=False, show_progress=False):
+    basebands = []
+    frames = None
+    if render:
+        frames = []
+    
+    t_offset = 0
+    for angle in (tqdm(steering_angles) if show_progress else steering_angles):
+        baseband, end_of_last_pulse, frame = frame_simulation(target_list, hpbw_az_deg, hpbw_el_deg, t_offset, steering_angle_deg=angle, include_noise=include_noise, render=render)
+        basebands.append(baseband)
+        t_offset += end_of_last_pulse
+        if render:            
+            frames.append(frame)
+
+    return (basebands, frames)
+
+
+hpbw_el_deg = 27
+hpbw_az_deg = 88
+
+max_abs_angle_deg = 55
+angle_step = 5
+steering_angles = np.arange(-max_abs_angle_deg, max_abs_angle_deg + angle_step, angle_step)
 
 target_list = [
     dict(
-        location=(10, 0, 0),
-        speed=(5, 0, 0),
+        location=(10, -5, 0),
+        speed=(0, 750, 0),
         rcs=10,
         phase=0,
-    )
+    ),
+    dict(
+        location=(-5, -5, 3),
+        speed=(350, -350, -350),
+        rcs=10,
+        phase=0,
+    ),
 ]
 
-plot_radar_scene(radar, target_list, show_pattern=False, pulse_idx=None)
+basebands, frames = sweep_simulation(target_list, hpbw_az_deg, hpbw_el_deg, steering_angles, include_noise=False, render=True, show_progress=True)
+save_frames_mp4(frames)
