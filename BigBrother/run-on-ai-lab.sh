ec04e4e217af1e8e251374344049cfcd9cd6eb91
Karl Andersen <brandt123kba@gmail.com>
2025-10-22 16:17:00 +0200

    changed ressource needs for ai-lab

--- BigBrother/run-on-ai-lab.sh
+++ BigBrother/run-on-ai-lab.sh
@@ -2,7 +2,8 @@
 
 uv sync
 
-srun --gres=gpu:1 singularity exec --nv \
+#Maximum at ai-lab --mem=24G --cpus-per-task=15 --gres=gpu:8
+srun --mem=12G --cpus-per-task=8 --gres=gpu:4 singularity exec --nv \
      -B ~/ESD7-Project/BigBrother:/scratch/BigBrother \
      /ceph/container/tensorflow/tensorflow_24.09.sif \
      /bin/bash -c "source /scratch/BigBrother/.venv/bin/activate && python /scratch/BigBrother/src/main.py"
