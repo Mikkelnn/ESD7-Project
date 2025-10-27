e9c0a49625cccf9c86edc84ca1d9dcfe9519a0cd
Karl Andersen <brandt123kba@gmail.com>
2025-10-27 16:14:20 +0100

    Added Generel path and added example of a CNN model

--- BigBrother/src/model.py
+++ BigBrother/src/model.py
@@ -2,7 +2,7 @@ from tensorflow.keras.layers import Dropout, MaxPooling2D, Convolution2D, Flatte
 from tensorflow.keras.models import Sequential
 
 def defineModel():
-    #TODO Potentially use tanh activation
+
     model = Sequential([
         Convolution2D(filters=8, kernel_size=(3,3), activation='relu', padding="same", input_shape=(1024, 256, 1)),
         MaxPooling2D((2,2), strides=(2,2)), #Half feature set
@@ -14,7 +14,7 @@ def defineModel():
         Flatten(),
         Dense(256, activation='relu'),
         Dense(256, activation='relu'),
-        Dense(2, activation='relu'),
+        Dense(2, activation='sigmoid'), #TODO Potentially use tanh activation of -1 to 1
     ])
      
     #model = Sequential([
