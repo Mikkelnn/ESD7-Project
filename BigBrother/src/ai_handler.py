be4ba710a633554f88b64e86a53b254322bdd861
Mikkel <jensenen@live.dk>
2025-10-28 16:27:47 +0100

    fix space error maybe...

--- BigBrother/src/ai_handler.py
+++ BigBrother/src/ai_handler.py
@@ -313,8 +313,8 @@ class AiHandler():
         if shuffle:
             dataset = dataset.shuffle(len(data_files), seed=seed)
 
-        # dataset = dataset.batch(batch_size).prefetch(self.tf.data.AUTOTUNE)
-        dataset = dataset.batch(batch_size)
+        dataset = dataset.batch(batch_size).prefetch(self.tf.data.AUTOTUNE)
+        # dataset = dataset.batch(batch_size)
 
         self.log.info(f"Finished loding training data....")
         
