a8281945f9e19eb1185a16298aa9fe468fdb99c7
Karl Andersen <brandt123kba@gmail.com>
2025-10-28 13:23:06 +0100

    changed hardware requests

--- BigBrother/run-on-ai-lab.sh
+++ BigBrother/run-on-ai-lab.sh
@@ -4,6 +4,6 @@ uv sync
 
 #Maximum at ai-lab --mem=24G --cpus-per-task=15 --gres=gpu:8
 srun --mem=24G --cpus-per-task=15 --gres=gpu:4 singularity exec --nv \
-     -B ~/ESD7-Project/BigBrother:/scratch/BigBrother \
+     -B ~/:/scratch \
      /ceph/container/tensorflow/tensorflow_24.09.sif \
      /bin/bash -c "source /scratch/ESD7-Project/BigBrother/.venv/bin/activate && python /scratch/ESD7-Project/BigBrother/src/main.py"
