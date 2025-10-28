0f40c8c8436cc3ebf1e702c7ade1a01751232d0c
Karl Andersen <brandt123kba@gmail.com>
2025-10-28 13:45:22 +0100

    Bind cores and no multithread

--- BigBrother/run-on-ai-lab.sh
+++ BigBrother/run-on-ai-lab.sh
@@ -3,13 +3,14 @@
 uv sync
 
 #Maximum at ai-lab --mem=24G --cpus-per-task=15 --gres=gpu:8
-srun --mem=24G --cpus-per-task=15 --gres=gpu:4 --hint=nomultithread singularity exec --nv \
+srun --mem=24G --cpus-per-task=8 --gres=gpu:1 singularity exec --nv \
      -B ~/:/scratch \
      /ceph/container/tensorflow/tensorflow_24.09.sif \
-     /bin/bash -c "source /scratch/ESD7-Project/BigBrother/.venv/bin/activate && python /scratch/ESD7-Project/BigBrother/src/main.py" \
      /bin/bash -lc '
        # Make CPU count explicit for libs inside the container
        export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
        export TF_NUM_INTRAOP_THREADS=${SLURM_CPUS_PER_TASK}
        export TF_NUM_INTEROP_THREADS=2
-     '
+     ' \
+     /bin/bash -c "source /scratch/ESD7-Project/BigBrother/.venv/bin/activate && python /scratch/ESD7-Project/BigBrother/src/main.py"
+     
