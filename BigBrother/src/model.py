e7725243cae11c7ad24201c282c386e7ca7ca7af
Karl Andersen <brandt123kba@gmail.com>
2025-10-29 12:46:35 +0100

    Sigmoid for all model layers

--- BigBrother/src/model.py
+++ BigBrother/src/model.py
@@ -4,16 +4,16 @@ from tensorflow.keras.models import Sequential
 def defineModel():
 
     model = Sequential([
-        Convolution2D(filters=8, kernel_size=(3,3), activation='relu', padding="same", input_shape=(1024, 256, 1)),
+        Convolution2D(filters=8, kernel_size=(3,3), activation='sigmoid', padding="same", input_shape=(1024, 256, 1)),
         MaxPooling2D((2,2), strides=(2,2)), #Half feature set
-        Convolution2D(filters=16, kernel_size=(3,3), activation='relu', padding="same"), 
+        Convolution2D(filters=16, kernel_size=(3,3), activation='sigmoid', padding="same"), 
         MaxPooling2D((2,2), strides=(2,2)), #Half feature set
-        Convolution2D(filters=32, kernel_size=(3,3), activation='relu', padding="same"),
+        Convolution2D(filters=32, kernel_size=(3,3), activation='sigmoid', padding="same"),
         MaxPooling2D((2,2), strides=(2,2)), #Half feature set
-        Convolution2D(filters=64, kernel_size=(3,3), activation='relu', padding="same"),
+        Convolution2D(filters=64, kernel_size=(3,3), activation='sigmoid', padding="same"),
         Flatten(),
-        Dense(256, activation='relu'),
-        Dense(256, activation='relu'),
+        Dense(256, activation='sigmoid'),
+        Dense(256, activation='sigmoid'),
         Dense(2, activation='sigmoid'), #TODO Potentially use tanh activation of -1 to 1
     ])
 
