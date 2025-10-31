697879bf32f5041a15863fe0a1d266035ff77701
Karl Andersen <brandt123kba@gmail.com>
2025-10-31 12:20:44 +0100

    Updated CFAR ROC

--- mathing/ROC calc/main.py
+++ mathing/ROC calc/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm, f, ncf
 
 # Parameters
-SNR_dB = 5
+SNR_dB = 10
 SNR = 10**(SNR_dB/10)
 N = 16
 pfa = np.linspace(0, 1, 400)
@@ -15,14 +15,9 @@ def pd_np_coherent(pfa, snr):
 #    return norm.sf(norm.isf(pfa) - np.sqrt(mismatch*snr))
 
 def pd_cafar(pfa, snr, N, cfar_loss_dB=1.5):
-    rho_eff = snr / (10**(cfar_loss_dB/10))  # optional CFAR loss (1–2 dB typical)
-    dfn, dfd = 2, 2*N
 
-    # Threshold tau from P_FA using central F quantile
-    tau = f.isf(pfa, dfn, dfd)  # inverse survival = 1 - CDF
+    pd = ((1+snr)/(pfa**(-1/N)+snr))**N
 
-    # P_D from noncentral F survival at same threshold
-    pd = ncf.sf(tau, dfn, dfd, 2*rho_eff)
     return pd
 
 def pd_ml(pfa, snr, kappa=0.95):
