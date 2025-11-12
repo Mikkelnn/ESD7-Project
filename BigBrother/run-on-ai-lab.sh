07148253ae162e7f86c1605c51bd502f9662fb6d
Mikkel <jensenen@live.dk>
2025-11-12 08:42:35 +0100

    run sbatch according to documentation

--- BigBrother/run-on-ai-lab.sh
+++ BigBrother/run-on-ai-lab.sh
@@ -2,14 +2,15 @@
 
 uv sync
 
+sbatch run_batch.sh
+
 #Maximum at ai-lab --mem=24G --cpus-per-task=15 --gres=gpu:8
 #srun --mem=96G --cpus-per-task=60 --gres=gpu:4 --time=04:00:00
-sbatch --mem=24G --cpus-per-task=15 --gres=gpu:1 --time=04:00:00 singularity exec --nv \
-     -B ~/:/scratch \
-     /ceph/container/tensorflow/tensorflow_24.09.sif \
-     /bin/bash -lc ' \
-       export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-       export TF_NUM_INTRAOP_THREADS=${SLURM_CPUS_PER_TASK}
-       export TF_NUM_INTEROP_THREADS=2
-       source /scratch/ESD7-Project/BigBrother/.venv/bin/activate && python /scratch/ESD7-Project/BigBrother/src/main.py
-     '
+# sbatch --mem=24G --cpus-per-task=15 --gres=gpu:1 --time=04:00:00 singularity exec --nv \
+#      -B ~/:/scratch \
+#      /ceph/container/tensorflow/tensorflow_24.09.sif \
+#      /bin/bash -lc ' \
+#        export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+#        export TF_NUM_INTRAOP_THREADS=${SLURM_CPUS_PER_TASK}
+#        export TF_NUM_INTEROP_THREADS=2
+#        source /scratch/ESD7-Project/BigBrother/.venv/bin/activate && python /scratch/ESD7-Project/BigBrother/src/main.py
---
+++ BigBrother/run_batch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=space-debris
+#SBATCH --output=my_job.out
+#SBATCH --error=my_job.err
+#SBATCH --mem=24G
+#SBATCH --cpus-per-task=15
+#SBATCH --gres=gpu:1
+#SBATCH --time=04:00:00
+
+# Run Python script in container
+singularity exec --nv \
+     -B ~/:/scratch \
+     /ceph/container/tensorflow/tensorflow_24.09.sif \
+     /bin/bash -lc ' \
+       export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+       export TF_NUM_INTRAOP_THREADS=${SLURM_CPUS_PER_TASK}
+       export TF_NUM_INTEROP_THREADS=2
+       source /scratch/ESD7-Project/BigBrother/.venv/bin/activate && python /scratch/ESD7-Project/BigBrother/src/main.py
