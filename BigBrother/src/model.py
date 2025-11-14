42ff76d28a252c5d4a675d057606ab2cd4784d3f
Mikkel <jensenen@live.dk>
2025-11-14 14:46:03 +0100

    single layer convelution and two dense

--- BigBrother/src/model.py
+++ BigBrother/src/model.py
@@ -5,6 +5,20 @@ def defineModel(output_size):
 
     model = Sequential([
         Input(shape=(1024, 256, 1)),
+        Convolution2D(filters=2, kernel_size=(2, 2), activation='relu', padding="same"),
+        Flatten(),
+        Dense(512, activation='relu'),
+        Dense(256, activation='relu'),
+        Dense(output_size, activation='sigmoid'), #TODO Potentially use tanh activation of -1 to 1
+    ])
+
+    return model
+
+
+def defineModel_bigCNN(output_size):
+
+    model = Sequential([
+        Input(shape=(1024, 256, 1)),
         Convolution2D(filters=1, kernel_size=(80, 20), activation='relu', padding="same"),
         MaxPooling2D((2,2), strides=(2,2)), #Half feature set
         Convolution2D(filters=16, kernel_size=(40,10), activation='relu', padding="same"), 
