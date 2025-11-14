b3b87b728687ee2d1d32d8653adbfb0c150f88ac
Mikkel <jensenen@live.dk>
2025-11-14 09:30:40 +0100

    new model only dense layers

--- BigBrother/src/model.py
+++ BigBrother/src/model.py
@@ -4,6 +4,19 @@ from tensorflow.keras.models import Sequential
 def defineModel(output_size):
 
     model = Sequential([
+        Dense(256, input_shape=(1024, 256, 1), activation='sigmoid'),
+        Dense(256, activation='sigmoid'),
+        Dense(256, activation='sigmoid'),
+        Dense(256, activation='sigmoid'),
+        Dense(256, activation='sigmoid'),
+        Dense(output_size, activation='sigmoid'), #TODO Potentially use tanh activation of -1 to 1
+    ])
+
+    return model
+
+def defineModel_convelution(output_size):
+
+    model = Sequential([
         Convolution2D(filters=8, kernel_size=(3,3), activation='sigmoid', padding="same", input_shape=(1024, 256, 1)),
         MaxPooling2D((2,2), strides=(2,2)), #Half feature set
         Convolution2D(filters=16, kernel_size=(3,3), activation='sigmoid', padding="same"), 
