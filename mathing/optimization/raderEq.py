57498f90e0b2c69e57a0d9e78ba270131e8aa496
MhawnSendes <jtheil22@student.aau.dk>
2025-12-03 14:29:27 +0100

    I am mentally ill

--- mathing/optimization/raderEq.py
+++ mathing/optimization/raderEq.py
@@ -1,20 +1,27 @@
+# example: elementwise -> returns (2,1)
 import numpy as np
-import math
+from scipy.constants import k
 
-def radar_received_power(Pt, Gt, Gr, wavelength, sigma, R):
-    """Calculate received power using the radar range equation."""
-    return (Pt * Gt * Gr * wavelength**2 * sigma) / ((4 * math.pi)**3 * R**4)
+Pt=10; Gt=12.5; Gr=Gt; c=3e8; freq=75e9
+wavelength = c/freq
+const = Pt * Gt * Gr * wavelength**2 / ((4*np.pi)**3)
 
-# Example usage:
-Pt = 10         # 10 W transmit power
-Gt = 12.5       # (10.97 dBi) transmit gain (linear)
-Gr = Gt         # receive gain (linear)
-freq = 75e9     # 75 GHz
-c = 3e8         # speed of light (m/s)
-wavelength = c / freq
-sigma = 3.47 * 10**(-6) # 1 m² RCS
-R = 1e3          # 1 km
+# to get 4 values (2x2) use broadcasting or outer product
+sigma = np.array([3.47e-6 , 0.035])[:, None]   # shape (2,1)
+R     = np.array([100.0, 2000.0])[None, :]  # shape (1,2)
+Pr = const * sigma / (R**4)  # shape (2,2) -> 4 combinations
 
-Pr = radar_received_power(Pt, Gt, Gr, wavelength, sigma, R)
-dbm_r = 10 * np.log10(Pr) + 30
-print(f"Received Power: {dbm_r} dbm")
+PrdBm = 10 * np.log10(Pr) + 30
+
+print(f"Received power (dBm): {PrdBm}")
+
+B = 20e6
+
+T_low = 253.15
+T_high = 353.15
+N_low = k * T_low * B       # W
+N_high = k * T_high * B     # W
+N_low_dBm = 10 * np.log10(N_low) + 30
+N_high_dBm = 10 * np.log10(N_high) + 30
+print(f"Noise floor at {T_low} K: {N_low_dBm} dBm")
+print(f"Noise floor at {T_high} K: {N_high_dBm} dBm")
