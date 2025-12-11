84193688698669728b3282922a415ffce1c1a97c
Mikkel <mikkel@norre.cc>
2025-12-11 07:39:21 +0000

    fix runner after udate of singularity

--- BigBrother/run_batch.sh
+++ BigBrother/run_batch.sh
@@ -9,12 +9,20 @@
 #SBATCH --time=12:00:00
 
 # Run Python script in container
-singularity exec --nv \ 
-    -B ~/:/scratch \
-    /ceph/container/tensorflow/tensorflow_24.09.sif \
-    /bin/bash -lc ' \
-      export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-      export TF_NUM_INTRAOP_THREADS=${SLURM_CPUS_PER_TASK}
-      export TF_NUM_INTEROP_THREADS=2
-      source /scratch/ESD7-Project/BigBrother/.venv/bin/activate && python /scratch/ESD7-Project/BigBrother/src/main.py
-    '
+singularity exec --nv -B ~/:/scratch \
+  /ceph/container/tensorflow/tensorflow_24.09.sif \
+  /bin/bash -lc "export OMP_NUM_THREADS=\${SLURM_CPUS_PER_TASK}; \
+  export TF_NUM_INTRAOP_THREADS=\${SLURM_CPUS_PER_TASK}; \
+  export TF_NUM_INTEROP_THREADS=2; \
+  source /scratch/ESD7-Project/BigBrother/.venv/bin/activate; \
+  python /scratch/ESD7-Project/BigBrother/src/main.py"
+
+# singularity exec --nv \ 
+#     -B ~/:/scratch \
+#     /ceph/container/tensorflow/tensorflow_24.09.sif \
+#     /bin/bash -lc ' \
+#       export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+#       export TF_NUM_INTRAOP_THREADS=${SLURM_CPUS_PER_TASK}
+#       export TF_NUM_INTEROP_THREADS=2
+#       source /scratch/ESD7-Project/BigBrother/.venv/bin/activate && python /scratch/ESD7-Project/BigBrother/src/main.py
+#     '
