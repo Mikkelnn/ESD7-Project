1b20af5ea1ec4576d0359eac385c8fcea6923ce9
Mikkel <mikkel@norre.cc>
2026-01-18 13:31:48 +0100

    fix label

--- BigBrother/src/main-localize.py
+++ BigBrother/src/main-localize.py
@@ -149,7 +149,8 @@ def main():
 
             def loader_func_label(f): 
                 label = np.load(f) # shape (2,) → [range, velocity]
-                return np.array([max_range * label[0], max_velocity * label[1]])  # scale to physical units
+                # return np.array([max_range * label[0], max_velocity * label[1]])  # scale to physical units
+                return { "range_m": max_range * label[0], "vel_mps": max_velocity * label[1] }
                 # return np.array([1,0]) if (sum(label) == 0) else np.array([0,1])
                 
 
