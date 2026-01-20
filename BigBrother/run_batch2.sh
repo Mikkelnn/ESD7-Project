88ad1ae7ffcd09ce5ffc54325edcbfd2693504be
Mikkel <mikkel@norre.cc>
2026-01-20 17:42:04 +0100

    run

--- BigBrother/run_batch2.sh
+++ BigBrother/run_batch2.sh
@@ -15,4 +15,4 @@ singularity exec --nv -B ~/:/scratch \
   export TF_NUM_INTRAOP_THREADS=\${SLURM_CPUS_PER_TASK}; \
   export TF_NUM_INTEROP_THREADS=2; \
   source /scratch/ESD7-Project/BigBrother/.venv/bin/activate; \
-  python /scratch/ESD7-Project-2/BigBrother/src/main-localize.py"
+  python /scratch/ESD7-Project/BigBrother/src/main-localize.py"
